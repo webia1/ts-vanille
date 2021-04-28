16b141b5a7bc48a2b3c31da571364f1a9a2f4171
Erkan Buelbuel <webia1.github@gmail.com>
2021-04-28 13:39:33 +0200

    Types++

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+const arr: Array<string> = ["s", "t"];
+
+let someAny: any;
+
+function greetMe(name: string): string {
+  return `Hi ${name}!`;
+}
+
+type Point = { x: number; y: number };
+
+function printC(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+printC({ x: 3, y: 7 });
+
+type Id = number | string; // <-- Id is a union type
+
+function getId(id: Id): string {
+  if (typeof id === "string") {
+    id = id.toUpperCase();
+  }
+  return String(id);
+}
+
+console.log(typeof getId(4), getId("hallo"));
